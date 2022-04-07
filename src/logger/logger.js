1c7358ed6c030a7b73ab56b2f4dc7251bcef4305
shikhasharma0 <shikha0252@gmail.com>
2022-04-07 12:47:15 +0530

    Assignment Done

--- src/logger/logger.js
+++ src/logger/logger.js
@@ -1,6 +1,6 @@
 
 let welcome = function() {
- console.log('Welcome to my application. I am <name> and a part of FunctionUp Uraninam cohort.')
+ console.log('Welcome to my application. I am <Shikha Sharma> and a part of FunctionUp Uraninam cohort.')
 }
 
 
