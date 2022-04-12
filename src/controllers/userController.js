edeb21d79cc6b1452e935c7db8c28d65dd426ba8
shikhasharma0 <shikha0252@gmail.com>
2022-04-12 08:40:29 +0530

    Assigment Done

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,15 +1,16 @@
-const UserModel= require("../models/userModel")
 
-const createUser= async function (req, res) {
+const Userbook= require("../models/userModel")
+
+const createBook= async function (req, res) {
     let data= req.body
-    let savedData= await UserModel.create(data)
+    let savedData= await Userbook.create(data)
     res.send({msg: savedData})
 }
 
-const getUsersData= async function (req, res) {
-    let allUsers= await UserModel.find()
-    res.send({msg: allUsers})
+const getBooks= async function (req, res) {
+    let allbooks= await Userbook.find()
+    res.send({msg: allbooks})
 }
 
-module.exports.createUser= createUser
-module.exports.getUsersData= getUsersData
+module.exports.createBook= createBook
+module.exports.getBooks= getBooks
