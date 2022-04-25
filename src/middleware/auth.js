ee979ee2fc81343b61544efac3183690b31ac00b
shikhasharma0 <shikha0252@gmail.com>
2022-04-25 17:57:42 +0530

    Assignment 22-04-2022

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,30 +3,45 @@ const router = require("../routes/route");
 const authenticate = function(req, res, next) {
     //check the token in request header
     //validate this token
-    let token = req.header["X-Auth-token"];
-    let (!token)= req.header["x-auth-token"];
-    if (!token) return res.send({status: true, msg: "token must be persent"});
-else{
-    next()}
+    try {
+      token = req.headers["x-auth-token"];
+    if (!token) {
+      return res.send({ status: false, msg: "Token must be present" });
+    }
+
+    let verifyUser = jwt.verify(token, "FunctionUp-Uranium");
+
+    if (!verifyUser) {
+      return res.send({ status: false, msg: "Token is incorrect" });
+    }
+    next();
+  } catch (err) {
+    res.status(400).send({ msg: err })
+  }
+  
 }
 
 
 const authorise = function(req, res, next) {
 
     // comapre the logged in user's id and the id in request
-    let token = req.headers["x-auth-token"];
-    let decodedToken = jwt.verify(token, "functionup-thorium");
-    let userToBeModified = req.params.userId;
-    let userLoggedIn = decodedToken.userId;
-    if (userToBeModified != userLoggedIn)
-      return res.send({
-        status: false,
-        msg: "User logged is not allowed to modify the requested users data",
-      });
-  
-    next();
-  
-  };
+
+      try {
+        let token = req.headers["x-auth-token"];
+        let decodedToken = jwt.verify(token, "FunctionUp-Uranium")
+        let userToBeModified = req.params.userId
+        let userLoggedIn = decodedToken.userId
+        if (userToBeModified != userLoggedIn) {
+          return res.send({ 
+            status: false, 
+            msg: 'User logged is not allowed to modify the requested users data' })
+        } else {
+          next();
+        }
+      } catch (err) {
+        res.status(400).send({ msg: err })
+      }
+}
 
 
 module.exports.authenticate = authenticate
